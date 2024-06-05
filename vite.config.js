c09e7029c1e651f8515e1655dabc97b962a84f92
marsuii <marshagrasis@stellamaryscoe.edu.in>
2024-06-05 13:37:25 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -11,25 +11,14 @@ import rehypeImgSize from 'rehype-img-size';
 import rehypeSlug from 'rehype-slug';
 import rehypePrism from '@mapbox/rehype-prism';
 
+
 const isStorybook = process.argv[1]?.includes('storybook');
 
 export default defineConfig({
-  base: '/marsuii.github.io/', // Base path set to GitHub Pages URL
   assetsInclude: ['**/*.glb', '**/*.hdr', '**/*.glsl'],
   build: {
     assetsInlineLimit: 1024,
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
   },
-  
   server: {
     port: 7777,
   },
